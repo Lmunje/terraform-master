5f3637925ba6864151a0b32889592cdcba16619b
Lionel Munje <46854325+Lmunje@users.noreply.github.com>
2024-09-06 19:18:20 -0400

    Update createInstance.tf

--- Terraform Variables Detailed Explanation/createInstance.tf
+++ Terraform Variables Detailed Explanation/createInstance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "demoinstnce"
+    Name = "demoinstance"
   }
 
   security_groups = var.Security_Group
