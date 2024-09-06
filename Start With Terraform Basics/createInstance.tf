dcc93a21e030f690304fcd1d07d78688815b1a9a
Lionel Munje <46854325+Lmunje@users.noreply.github.com>
2024-09-06 19:00:27 -0400

    Update createInstance.tf

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,10 +1,4 @@
 
-provider "aws" {
-  access_key = "AKIAR3HUOFMPCEHFMWVB"
-  secret_key = "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"
-  region =  "us-east-2"
-}
-
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
   ami           = "ami-05803413c51f242b7"
