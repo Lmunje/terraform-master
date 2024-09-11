46d3a80be0538dadf74a43e53c52f1355efc0d74
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-11 17:07:31 -0400

    latest ubuntu version

--- Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createInstance.tf
@@ -3,11 +3,11 @@ data "aws_availability_zones" "available" {}
 
 data "aws_ami" "latest-ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
 
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
