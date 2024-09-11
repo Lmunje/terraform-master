bebd2b161f2f6d14792e0ef4d69ea4f308339fa7
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-11 16:37:17 -0400

    s3 name change

--- Terraform Concepts Building Blocks/Demo DataSource/backend.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
-        bucket = "tf-state-98fty"
+        bucket = "tf-state-98fty-01"
         key    = "development/terraform_state"
         region = "us-east-1"
     }
 }
 
-Testing
+
