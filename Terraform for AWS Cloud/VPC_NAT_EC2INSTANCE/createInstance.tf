252c46a59fa655a34ee6b6f015ab8a48bf5c12ea
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-12 16:28:58 -0400

    ec2 instances

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/createInstance.tf
@@ -1,16 +1,16 @@
 
-resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
+resource "aws_key_pair" "lionel_key" {
+    key_name = "lionel_key"
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.levelup_key.key_name
+  key_name      = aws_key_pair.lionel_key.key_name
 
-  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
-  subnet_id = aws_subnet.levelupvpc-public-2.id
+  vpc_security_group_ids = [aws_security_group.allow-lionel-ssh.id]
+  subnet_id = aws_subnet.lionelvpc-public-2.id
 
   tags = {
     Name = "custom_instance"
--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/nat.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/nat.tf
@@ -1,38 +1,38 @@
 #Define External IP 
-resource "aws_eip" "levelup-nat" {
+resource "aws_eip" "lionel-nat" {
   vpc = true
 }
 
-resource "aws_nat_gateway" "levelup-nat-gw" {
-  allocation_id = aws_eip.levelup-nat.id
-  subnet_id     = aws_subnet.levelupvpc-public-1.id
-  depends_on    = [aws_internet_gateway.levelup-gw]
+resource "aws_nat_gateway" "lionel-nat-gw" {
+  allocation_id = aws_eip.lionel-nat.id
+  subnet_id     = aws_subnet.lionelvpc-public-1.id
+  depends_on    = [aws_internet_gateway.lionel-gw]
 }
 
-resource "aws_route_table" "levelup-private" {
-  vpc_id = aws_vpc.levelupvpc.id
+resource "aws_route_table" "lionel-private" {
+  vpc_id = aws_vpc.lionelvpc.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.levelup-nat-gw.id
+    nat_gateway_id = aws_nat_gateway.lionel-nat-gw.id
   }
 
   tags = {
-    Name = "levelup-private"
+    Name = "lionel-private"
   }
 }
 
 # route associations private
 resource "aws_route_table_association" "level-private-1-a" {
-  subnet_id      = aws_subnet.levelupvpc-private-1.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.lionelvpc-private-1.id
+  route_table_id = aws_route_table.lionel-private.id
 }
 
 resource "aws_route_table_association" "level-private-1-b" {
-  subnet_id      = aws_subnet.levelupvpc-private-2.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.lionelvpc-private-2.id
+  route_table_id = aws_route_table.lionel-private.id
 }
 
 resource "aws_route_table_association" "level-private-1-c" {
-  subnet_id      = aws_subnet.levelupvpc-private-3.id
-  route_table_id = aws_route_table.levelup-private.id
+  subnet_id      = aws_subnet.lionelvpc-private-3.id
+  route_table_id = aws_route_table.lionel-private.id
 }
