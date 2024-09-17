fc2a38fd3226f1f36f4e8cb87ffd1db83df8e056
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-17 18:14:18 -0400

    update rds to latest

--- Terraform for AWS Cloud II/AWS RDS/rds.tf
+++ Terraform for AWS Cloud II/AWS RDS/rds.tf
@@ -1,14 +1,14 @@
-#RDS Resources
+# RDS Resources
 resource "aws_db_subnet_group" "mariadb-subnets" {
   name        = "mariadb-subnets"
   description = "Amazon RDS subnet group"
   subnet_ids  = [aws_subnet.lionelvpc-private-1.id, aws_subnet.lionelvpc-private-2.id]
 }
 
-#RDS Parameters
+# RDS Parameters
 resource "aws_db_parameter_group" "lionel-mariadb-parameters" {
   name        = "lionel-mariadb-parameters"
-  family      = "mariadb10.4"
+  family      = "mariadb10.6"  # Use family corresponding to the engine version
   description = "MariaDB parameter group"
 
   parameter {
@@ -17,24 +17,24 @@ resource "aws_db_parameter_group" "lionel-mariadb-parameters" {
   }
 }
 
-#RDS Instance properties
+# RDS Instance properties
 resource "aws_db_instance" "lionel-mariadb" {
   allocated_storage       = 20             # 20 GB of storage
   engine                  = "mariadb"
-  engine_version          = "10.11"
-  instance_class          = "db.t2.micro"  # use micro if you want to use the free tier
+  engine_version          = "10.6"         # Switch to a more widely supported version
+  instance_class          = "db.t3.micro"  # Use t3.micro for compatibility
   identifier              = "mariadb"
   db_name                 = "mariadb"
-  username                = "root"           # username
-  password                = "mariadb141"     # password
+  username                = "root"         # Username
+  password                = "mariadb141"   # Password
   db_subnet_group_name    = aws_db_subnet_group.mariadb-subnets.name
   parameter_group_name    = aws_db_parameter_group.lionel-mariadb-parameters.name
-  multi_az                = "false"            # set to true to have high availability: 2 instances synchronized with each other
+  multi_az                = "false"        # Single AZ for cost-saving
   vpc_security_group_ids  = [aws_security_group.allow-mariadb.id]
   storage_type            = "gp2"
-  backup_retention_period = 30                                          # how long you’re going to keep your backups
-  availability_zone       = aws_subnet.lionelvpc-private-1.availability_zone # prefered AZ
-  skip_final_snapshot     = true                                        # skip final snapshot when doing terraform destroy
+  backup_retention_period = 30             # 30-day backup retention
+  availability_zone       = aws_subnet.lionelvpc-private-1.availability_zone
+  skip_final_snapshot     = true           # Skip final snapshot during destroy
   
   tags = {
     Name = "lionel-mariadb"
@@ -44,4 +44,3 @@ resource "aws_db_instance" "lionel-mariadb" {
 output "rds" {
   value = aws_db_instance.lionel-mariadb.endpoint
 }
-
