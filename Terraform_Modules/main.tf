cfef6a97433e4d2ce7ab7ee8801a92f4bddd3267
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-22 10:27:40 -0400

    custom ami

--- Terraform_Modules/main.tf
+++ Terraform_Modules/main.tf
@@ -2,7 +2,7 @@ module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
     name    = "my-cluster"
-    ami     = "ami-04b3c39a8a1c62b76"
+    ami     = "ami-002271f2e358555c0"
     instance_type          = "t2.micro"
     subnet_id   = "subnet-0ab0d3ad4dde701a6"
 
