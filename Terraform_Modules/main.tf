9c5903c91de356b42625a3eec27b643dd39fe56e
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-22 10:47:00 -0400

    ami update

--- Terraform_Modules/main.tf
+++ Terraform_Modules/main.tf
@@ -2,7 +2,7 @@ module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
     name    = "my-cluster"
-    ami     = "ami-002271f2e358555c0"
+    ami     = "ami-0c55b159cbfafe1f0"
     instance_type          = "t2.micro"
     subnet_id   = "subnet-0ab0d3ad4dde701a6"
 
