4a329716035c6a64bd3749582591cb17425d8332
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-25 13:13:21 -0400

    update cs1

--- casestudy#1/module/vpc/vpc.tf
+++ casestudy#1/module/vpc/vpc.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "lionel_vpc" {
 #public Subnet 1
 resource "aws_subnet" "lionel_vpc_public_subnet_1" {
   vpc_id     = aws_vpc.lionel_vpc.id
-  cidr_block = var.lionel_VPC_PUBLIC_SUBNET1_CIDR_BLOCK
+  cidr_block = var.LIONEL_VPC_PUBLIC_SUBNET1_CIDR_BLOCK
   availability_zone = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = "true"
   tags = {
@@ -28,7 +28,7 @@ resource "aws_subnet" "lionel_vpc_public_subnet_1" {
 #public Subnet 2
 resource "aws_subnet" "lionel_vpc_public_subnet_2" {
   vpc_id     = aws_vpc.lionel_vpc.id
-  cidr_block = var.lionel_VPC_PUBLIC_SUBNET2_CIDR_BLOCK
+  cidr_block = var.LIONEL_VPC_PUBLIC_SUBNET2_CIDR_BLOCK
   availability_zone = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = "true"
   tags = {
@@ -39,7 +39,7 @@ resource "aws_subnet" "lionel_vpc_public_subnet_2" {
 # private subnet 1
 resource "aws_subnet" "lionel_vpc_private_subnet_1" {
   vpc_id     = aws_vpc.lionel_vpc.id
-  cidr_block = var.lionel_VPC_PRIVATE_SUBNET1_CIDR_BLOCK
+  cidr_block = var.LIONEL_VPC_PRIVATE_SUBNET1_CIDR_BLOCK
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
     Name = "${var.ENVIRONMENT}-lionel-vpc-private-subnet-1"
@@ -48,7 +48,7 @@ resource "aws_subnet" "lionel_vpc_private_subnet_1" {
 # private subnet 2
 resource "aws_subnet" "lionel_vpc_private_subnet_2" {
   vpc_id     = aws_vpc.lionel_vpc.id
-  cidr_block = var.lionel_VPC_PRIVATE_SUBNET2_CIDR_BLOCK
+  cidr_block = var.LIONEL_VPC_PRIVATE_SUBNET2_CIDR_BLOCK
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
     Name = "${var.ENVIRONMENT}-lionel-vpc-private-subnet-2"
