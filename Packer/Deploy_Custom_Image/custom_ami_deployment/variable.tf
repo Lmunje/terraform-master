87072ec1cb610e84a377f425f83b8189278031af
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-09-25 11:10:16 -0400

    terraform with packer

--- Packer/Deploy_Custom_Image/custom_ami_deployment/variable.tf
+++ Packer/Deploy_Custom_Image/custom_ami_deployment/variable.tf
@@ -1,7 +1,7 @@
 # Variable for Create Instance Module
 variable "public_key_path" {
   description = "Public key path"
-  default = "~/.ssh/levelup_key.pub"
+  default = "~/.ssh/lionel_key.pub"
 }
 
 variable "ENVIRONMENT" {
@@ -15,7 +15,7 @@ variable "AMI_ID" {
 }
 
 variable "AWS_REGION" {
-default = "us-east-2"
+default = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
