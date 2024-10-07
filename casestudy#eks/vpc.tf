b65fd0d18333358285ce68025873a525bd142a2b
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-10-07 14:58:14 -0400

    revert subnet changes

--- casestudy#eks/vpc.tf
+++ casestudy#eks/vpc.tf
@@ -1,23 +1,3 @@
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.13.0"
-
-#   name = "vpc-module-demo"
-#   cidr = "10.0.0.0/16"
-
-#   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-#   enable_nat_gateway = false
-#   enable_vpn_gateway = false
-
-#   tags = {
-#     Name = "${var.cluster-name}-vpc"
-#   }
-# }
-
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
@@ -32,16 +12,9 @@ module "vpc" {
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
-  # Tags for Kubernetes, ensuring correct roles for ELB
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = "1"
-  }
-
   tags = {
     Name = "${var.cluster-name}-vpc"
   }
 }
+
+
