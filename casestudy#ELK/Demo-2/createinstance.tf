ca9c5edc493b46ebeef3f707f8fc42b82ab9e4e0
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-10-07 18:33:11 -0400

    adding custom subnet to tf file

--- casestudy#ELK/Demo-2/createinstance.tf
+++ casestudy#ELK/Demo-2/createinstance.tf
@@ -1,111 +1,113 @@
-
 resource "aws_key_pair" "lionel_key" {
-    key_name = "lionel_key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
+    key_name    = "lionel_key"
+    public_key  = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_security_group" "allow_elk" {
-  name        = "allow_elk"
-  description = "All all elasticsearch traffic"
+    name        = "allow_elk"
+    description = "All all elasticsearch traffic"
 
-  # elasticsearch port
-  ingress {
-    from_port   = 9200
-    to_port     = 9200
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    # elasticsearch port
+    ingress {
+        from_port   = 9200
+        to_port     = 9200
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  # logstash port
-  ingress {
-    from_port   = 5043
-    to_port     = 5044
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    # logstash port
+    ingress {
+        from_port   = 5043
+        to_port     = 5044
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  # kibana ports
-  ingress {
-    from_port   = 5601
-    to_port     = 5601
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    # kibana ports
+    ingress {
+        from_port   = 5601
+        to_port     = 5601
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  # ssh
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    # ssh
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  # outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    # outbound
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 #Create AWS Instance
 resource "aws_instance" "MyFirstInstnace" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
-  instance_type = "m4.large"
-  availability_zone = "us-east-1a"
-  key_name      = aws_key_pair.lionel_key.key_name
+    ami                    = lookup(var.AMIS, var.AWS_REGION)
+    instance_type         = "m4.large"
+    availability_zone     = "us-east-1a"
+    key_name              = aws_key_pair.lionel_key.key_name
 
-  vpc_security_group_ids = [
-    aws_security_group.allow_elk.id,
-  ]
+    # Specify the subnet ID here
+    subnet_id             = "subnet-008797d78d60b5220"  # Add your subnet ID
 
-  depends_on = [aws_security_group.allow_elk]
-  
-  tags = {
-    Name = "custom_instance"
-  }
+    vpc_security_group_ids = [
+        aws_security_group.allow_elk.id,
+    ]
 
-  provisioner "file" {
-      source = "elasticsearch.yml"
-      destination = "/tmp/elasticsearch.yml"
-  }
+    depends_on = [aws_security_group.allow_elk]
 
-  provisioner "file" {
-      source = "kibana.yml"
-      destination = "/tmp/kibana.yml"
-  }
-
-  provisioner "file" {
-      source = "apache-01.conf"
-      destination = "/tmp/apache-01.conf"
-  }
+    tags = {
+        Name = "custom_instance"
+    }
 
     provisioner "file" {
-      source = "installELK.sh"
-      destination = "/tmp/installELK.sh"
-  }
+        source      = "elasticsearch.yml"
+        destination = "/tmp/elasticsearch.yml"
+    }
 
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x    /tmp/installELK.sh",
-      "sudo sed -i -e 's/\r$//' /tmp/installELK.sh",  # Remove the spurious CR characters.
-      "sudo /tmp/installELK.sh",
-    ]
-  }
+    provisioner "file" {
+        source      = "kibana.yml"
+        destination = "/tmp/kibana.yml"
+    }
 
-  connection {
-    host        = coalesce(self.public_ip, self.private_ip)
-    type        = "ssh"
-    user        = var.INSTANCE_USERNAME
-    private_key = file(var.PATH_TO_PRIVATE_KEY)
-  }
+    provisioner "file" {
+        source      = "apache-01.conf"
+        destination = "/tmp/apache-01.conf"
+    }
+
+    provisioner "file" {
+        source      = "installELK.sh"
+        destination = "/tmp/installELK.sh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "chmod +x    /tmp/installELK.sh",
+            "sudo sed -i -e 's/\r$//' /tmp/installELK.sh",  # Remove the spurious CR characters.
+            "sudo /tmp/installELK.sh",
+        ]
+    }
+
+    connection {
+        host        = coalesce(self.public_ip, self.private_ip)
+        type        = "ssh"
+        user        = var.INSTANCE_USERNAME
+        private_key = file(var.PATH_TO_PRIVATE_KEY)
+    }
 }
 
 resource "aws_eip" "ip" {
-  instance = aws_instance.MyFirstInstnace.id
+    instance = aws_instance.MyFirstInstnace.id
 }
 
 output "public_ip" {
-  value = aws_instance.MyFirstInstnace.public_ip 
+    value = aws_instance.MyFirstInstnace.public_ip 
 }
