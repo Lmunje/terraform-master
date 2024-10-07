1f5a7635e7b40319dbc14b9be242c42a9dd6e188
Lionel Munje <lionelmunje@Kirstens-MacBook-Pro.local>
2024-10-07 15:05:03 -0400

    update typo

--- casestudy#eks/vpc.tf
+++ casestudy#eks/vpc.tf
@@ -5,34 +5,27 @@ module "vpc" {
   name = "vpc-module-demo"
   cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  # Availability zones
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  # Subnets
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = [
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    map(aws_subnet.public_subnet.* : { cidr = element(var.public_subnets, aws_subnet.public_subnet.value.cidr_block) }, { map_entries = true }) {
-      map_key = title(cidr)
-      availability_zone = data.aws_availability_zones.available.names[cidr]
-      map_value {
-        # Enable auto-assign public IP here
-        map_key = "map_public_ip_on_launch"
-        map_value = true
-      }
-    }
-  ]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
-  # Tags for Kubernetes, ensuring correct roles for ELB
+  # Enable auto-assign public IP for public subnets
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"  = "1"  # For Kubernetes ELB role
+    "map_public_ip_on_launch" = "true"
   }
 
+  # Tags for private subnets for internal load balancers
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = "1"
   }
+
+  # Disable NAT and VPN gateways
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
     Name = "${var.cluster-name}-vpc"
